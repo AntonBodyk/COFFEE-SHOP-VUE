4687e4d922fe82fecd8e2170f5851e1785c820de
Anton <abodyk1516@gmail.com>
2023-02-24 10:58:33 +0200

    create timer

--- src/components/main/MainSales.vue
+++ src/components/main/MainSales.vue
@@ -4,19 +4,19 @@
             <div class="title">Left until the end of the promotion:</div>
             <div class="timer">
                 <div class="timer__block">
-                    <span id="days">30</span>
+                    <span id="days">{{ days }}</span>
                         days
                 </div>
                 <div class="timer__block">
-                    <span id="hours">20</span>
+                    <span id="hours">{{ hours }}</span>
                     hours
                 </div>
                 <div class="timer__block">
-                    <span id="minutes">56</span>
+                    <span id="minutes">{{ minutes }}</span>
                     minutes
                 </div>
                 <div class="timer__block">
-                    <span id="seconds">20</span>
+                    <span id="seconds">{{ seconds }}</span>
                     seconds
                 </div>
             </div>
@@ -25,13 +25,64 @@
  
 <script>
     export default {
+        data() {
+            return {
+                days: 0,
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+                loaded: false,
+                launchDate: new Date('30 May 2023')
+            }
+        },
+        computed: {
+            _seconds: () => 1000,
+            _minutes() {
+                return this._seconds * 60;
+            },
+            _hours() {
+                return this._minutes * 60;
+            },
+            _days() {
+                return this._hours * 24;
+            }
+        },
+        mounted(){
+            this.showRemaining();
+        },
+        methods: {
+            formatNumb(num) {
+                return num < 10 ? `0${num}` : num;
+            },
+            showRemaining() {
+                const timer = setInterval(() => {
+                const currDate = new Date();
+                const deadlineTime = this.launchDate - currDate;
 
+                if(deadlineTime < 0) {
+                    clearInterval(timer);
+                    return;
+                }
+
+                const days = Math.floor((deadlineTime / this._days));
+                const hours = Math.floor((deadlineTime % this._days) / this._hours);
+                const minutes = Math.floor((deadlineTime % this._hours) / this._minutes);
+                const seconds = Math.floor((deadlineTime % this._minutes) / this._seconds);
+
+                this.minutes = this.formatNumb(minutes);
+                this.seconds = this.formatNumb(seconds);
+                this.hours = this.formatNumb(hours);
+                this.days = this.formatNumb(days);
+                this.loaded = true;
+            }, 1000);
+        }
     }
+}
 </script>
  
 <style scoped>
 .promotion__timer{
-    margin: 30px 0;
+    margin: 30px 0 100px 0;
     width: 1000px;
 }
 .promotion__timer h2{
